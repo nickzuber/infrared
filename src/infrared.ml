50758842013a12f816f45ba8c179970621127d2f
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-01-05 15:37:50 -0500

    improved spec a bit

--- src/infrared.ml
+++ src/infrared.ml
@@ -1,8 +1,18 @@
-(* Core Infrared main shell *)
+(* Core Infrared shell for filtering and dispatching commands *)
 
 module InfraredShell : sig
+  val commands : command list
   val main : unit -> unit
 end = struct
+  
+  let commands = [
+    ParseCommand.command;
+    TypeCheckCommand.command;
+    WatchCommand.command;
+    HelpCommand.command;
+    VersionCommand.command;
+  ]
+
   let main () = 
     print_endline ("Placeholder")
 end
