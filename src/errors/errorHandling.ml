29eee98f886f110dac2def026afc1ef7a3ef076d
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-01-08 20:34:34 -0500

    typo

--- src/errors/errorHandling.ml
+++ src/errors/errorHandling.ml
@@ -4,7 +4,7 @@ let report ~msg ~level =
   | Level.High ->
     print_endline("🚨  Serious error: " ^ msg)
   | Level.Med ->
-      print_endline("❗ Oh boy.. ")
+      print_endline("❗ Oh boy.. " ^ msg)
   | Level.Low ->
       print_endline("⚠️  Ye be warned, " ^ msg)
 
