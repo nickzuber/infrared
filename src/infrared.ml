62607997a9bcadb17b7323c4ca42b44395b5de14
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-01-12 16:22:59 -0500

    parsing command line

--- src/infrared.ml
+++ src/infrared.ml
@@ -7,6 +7,7 @@ module InfraredShell : sig
   val commands : CommandSpec.t list
   val main : unit -> unit
   val greeting : unit -> unit
+  val failure : string -> unit
 end = struct
   let commands = [
     ParseCommand.command;
@@ -17,13 +18,21 @@ end = struct
 *)  
   ]
 
+  let failure msg = 
+    Printf.printf "🔥  Whoops, %s" msg
+
   let greeting () = 
     Printf.printf "%s%s"
       "✨  🚀  Infrared v1.0.1 — "
       "Fast light weight inferred static type checker in real time for JavaScript.\n\n"
 
   let main () = 
-    HelpCommand.exec commands
+    let argv = Array.to_list Sys.argv in
+    let command = match argv with
+    | [] -> failure "no args found whatsoever, shouldn't ever see this"
+    | prgm :: [] -> HelpCommand.exec commands
+    | prgm :: cmd :: rest -> VersionCommand.exec ()
+    in command
 end
 
 let _ = 
