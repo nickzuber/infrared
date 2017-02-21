0c13feb8319898145dc6c3bc9019da37423e9705
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-02-21 01:32:28 -0500

    some adjustments and comments

--- src/watch/diff.ml
+++ src/watch/diff.ml
@@ -2,22 +2,28 @@
 let get_mtime file =
   Unix.((stat file).st_mtime)
 
-let cache = Hashtbl.create 50
+let create_cache ?size:(size=50) =
+  Hashtbl.create size
 
+(*
+   Checks the cache for the last modified time of a given file.
+   Few scenarios:
+    - File exists
+      - Has been updated   => update cache; return true
+      - No changes         => return false
+    - File does not exist  => add to cache; return true
+ *)
 let check_diff file ~cache = 
   let file_mtime = get_mtime file in
   try
     let cached_mtime = Hashtbl.find cache file in
     if cached_mtime <> file_mtime then
-      Hashtbl.replace cache file file_mtime;
-      (* This file has been updated and needs to be rechecked *)
+      (Hashtbl.replace cache file file_mtime; true)
     else 
-      (* This file has not been changed *)
+      false
   with
     Not_found -> 
-      Hashtbl.add cache file file_mtime;
-      (* This file has not been tracked or found before, so we obviously
-       * need to be checked *)
+      (Hashtbl.add cache file file_mtime; true)
 
 
 
