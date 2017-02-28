1f69dadc361f78fd6e03f365569f3eb80b804b57
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-02-27 23:38:08 -0500

    polling and thread creation

--- src/watch/diff.ml
+++ src/watch/diff.ml
@@ -1,5 +1,5 @@
 
-let is_valid_file file =
+let is_file file =
   try
     let stat = Unix.stat file in 
     true (* We don't care about the stats, just the fact it found some *)
@@ -17,23 +17,14 @@ let get_mtime file =
 let create_cache ?size:(size=50) =
   Hashtbl.create size
 
-(* Checks the cache for the last modified time of a given file.
- * Few scenarios:
- *  - File exists
- *    - Has been updated   => update cache; return true
- *    - No changes         => return false
- *  - File does not exist  => add to cache; return true *)
-let has_diff file ~cache = 
-  let file_mtime = get_mtime file in
-  try
-    let cached_mtime = Hashtbl.find cache file in
-    if cached_mtime <> file_mtime then
+(* Checks the cache for the last modified time of a given file. 
+ * This function is intended to be used in the context of the
+ * `Hashtbl.iter` method, therefore are parameters will be the
+ * `key` and `value` which is our file name and m_time. *)
+let has_diff file cached_mtime = 
+  let current_mtime = get_mtime file in
+  if cached_mtime <> current_mtime then
       (Hashtbl.replace cache file file_mtime; true)
-    else 
-      false
-  with
-    Not_found -> 
-      (Hashtbl.add cache file file_mtime; true)
-
-
+  else 
+    false
 
---
+++ src/watch/poll.ml
@@ -0,0 +1,25 @@
+
+(* Similar to `Diff.has_diff`, this is used in conjunction
+ * with `Hashtbl.iter` so the parameters are to be assumed. *)
+let handle_diff file cached_mtime =
+  let is_stale = Diff.has_diff file cached_mtime in
+  if is_stale then
+    ignore ("this file needs to be parsed and checked again")
+  else
+    ignore ("do nothing")
+
+(* Checks every file in cache for diffs and handles as needed. *)
+let check_all_files cache =
+  Hashtbl.iter handle_diff cache
+
+(* This function polls file watching. *)
+let rec watch_all_files cache =
+  check_all_files cache;
+  Unix.sleep 5 (* Wait time between polling checks. *)
+
+(* Creates a new thread that begins to watch all the files in 
+* the given cache. Someone else is responsible for filling the 
+* cache. *)
+let create_watcher cache =
+  Thread.create watch_all_files cache
+
