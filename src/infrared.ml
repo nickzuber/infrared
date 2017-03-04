1b61fa4ed505164e6e9c3f2dbad7c028c649b726
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-03-04 18:07:52 -0500

    better cli parsing

--- src/infrared.ml
+++ src/infrared.ml
@@ -39,16 +39,23 @@ end = struct
            * to try and match commands like this because we cannot store custom
            * exec functions to the commands themselves, but rather to their
            * modules. I'm sure there's a "right" way to do this so keep thinking. *)
+          let parsedArgs = Fs.sanitize_args args in
+          let (flags', args') = parsedArgs in
           match command.name with
           | cmd when HelpCommand.spec.name = cmd -> HelpCommand.exec commands
           | cmd when VersionCommand.spec.name = cmd -> VersionCommand.exec ()
           | cmd when ParseCommand.spec.name = cmd -> 
             (match args with
             | [] -> reportCommandError "no arguments given for parsing."
-            | arg :: [] -> Ast.printAST (ParseCommand.exec ~args:[arg])
+            | arg :: [] -> 
+                Ast.printAST (ParseCommand.exec ~flags:flags' ~args:[arg])
             | _ -> 
-                Printf.printf "requested to parse the following files:\n";
-                Core.Std.List.iter ~f:(fun file -> Printf.printf "%s\n" file) args)
+                Printf.printf "args found\n";
+                Core.Std.List.iter ~f:(fun file -> Printf.printf "%s\n" file) args';
+                Printf.printf "\nflags found\n";
+                Core.Std.List.iter ~f:(fun file -> Printf.printf "%s\n" file) flags')
+          | cmd when TypeCheckCommand.spec.name = cmd ->
+              (ignore ("this"))
           | _ -> raise Not_found
         with Not_found ->
           reportCommandError "you've entered an invalid command.\n";
--- utils/fs.ml
+++ utils/fs.ml
@@ -2,3 +2,12 @@
 let extract_files args =
   []
 
+(* Check the first character to determine is flag or not. *)
+let is_flag arg =
+  let first = String.get arg 0 in
+  if first = '-' then true else false
+
+(* Separate arguments and flags and produce a tuple. *)
+let sanitize_args args =
+  List.partition is_flag args
+
