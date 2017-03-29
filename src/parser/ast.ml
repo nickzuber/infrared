56cd4a4727eab7dce4c22a4f0e6f61e77c7ac016
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-03-28 20:45:14 -0400

    ah much better

--- src/parser/ast.ml
+++ src/parser/ast.ml
@@ -12,20 +12,8 @@ module rec Loc : sig
 end = Loc
 
 and Program : sig
-  type t = (string * Type.t list)
+  type t = (string * Statement.t list)
 end = Program
-
-and Identifier : sig
-  type t = (string * Loc.t)
-end = Identifier
-
-and Expression : sig
-  type t
-end = Expression
-
-and Object : sig
-  type t
-end = Object
 
 and Literal : sig
   module Number : sig
@@ -39,24 +27,59 @@ and Literal : sig
   module String : sig
     type t = (string * Loc.t)
   end
+
+  type t = 
+    | Number of Number.t
+    | Boolean of Boolean.t
+    | String of String.t
+    | Null
 end = Literal
 
-and VariableDeclaration : sig
-  type t = {
-    declarations: t' list;
-    kind: var_t;
-  }
+and Statement : sig
+  module Identifier : sig
+    type t = (string * Loc.t)
+  end
 
-  and t' =
+  module Expression : sig
+    type t
+  end
+
+  module Object : sig
+    type t
+  end
+
+  module VariableDeclaration : sig
+    (* Multiple declarations could be handled by parsing into
+     * multiple `VariableDeclaration`? *)
+    type t = {
+      id: Identifier.t;
+      value: t';
+      kind: var_t;
+    }
+
+    and t' =
+      | Identifier of Identifier.t
+      | Function of Function.t
+      | Expression of Expression.t
+      | Literal of Literal.t
+      | Object of Object.t
+
+    and var_t =
+      | Var
+      | Let
+      | Const
+  end
+
+  type t = (t' * Loc.t)
+
+  type t' = 
     | Identifier of Identifier.t
-    | Function of Function.t
     | Expression of Expression.t
-    | Literal of Literal.t
     | Object of Object.t
+    | VariableDeclaration of VariableDeclaration.t
+end = Statement
 
-  and var_t =
-    | Var
-    | Let
-    | Const
-end = VariableDeclaration
+
+
+
 
