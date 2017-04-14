03ef8f9565972b2a871c15a725ab281e862df44f
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-04-14 19:39:19 -0400

    baby steps and removed old ast code

--- src/parser/ast.ml
+++ src/parser/ast.ml
@@ -151,113 +151,17 @@ and NamedObjectProperty : sig
   }
 end = NamedObjectProperty
 
-(*
-module rec Loc : sig
+and MethodDefinition : sig
   type t = {
-    line: int
+    body: FunctionBody.t;
   }
-end = Loc
+end = MethodDefinition
 
-and Program : sig
-  type t = (string * Statement.t list)
-end = Program
 
-and Literal : sig
-  module Number : sig
-    type t = (float * Loc.t)
-  end
 
-  module Boolean : sig
-    type t = (bool * Loc.t)
-  end
 
-  module String : sig
-    type t = (string * Loc.t)
-  end
 
-  type t = 
-    | Number of Number.t
-    | Boolean of Boolean.t
-    | String of String.t
-    | Null
-end = Literal
 
-and Statement : sig
-  module Identifier : sig
-    type t = (string * Loc.t)
-  end
-
-  module Expression : sig
-    module BlockExpression : sig
-      type t = (Statement.t list * Loc.t)
-    end
-
-    module StandardExpression : sig
-      type t
-    end
-
-    module CallExpression : sig
-      type t' = 
-        | Identifier of Identifier.t
-        | C
-
-      type t = {
-        arguments: Statement.t list;
-        callee: Identifier.t;
-      }
-    end
-
-    type t = 
-      | BlockExpression of BlockExpression.t
-      | StandardExpression of StandardExpression.t
-  end
-
-  module Function : sig
-    type t = (t' * Loc.t)
-
-    and t' = {
-      id: Identifier.t option;
-      params: Identifier.t list option;
-      body: Expression.BlockExpression.t;
-    }
-  end
-
-  module Object : sig
-    type t
-  end
-
-  module VariableDeclaration : sig
-    type t = (t' * Loc.t)
-
-    and t' = {
-      id: Identifier.t;
-      value: t'';
-      kind: var_t;
-    }
-
-    and t'' =
-      | Empty
-      | Identifier of Identifier.t
-      | Function of Function.t
-      | Expression of Expression.t
-      | Literal of Literal.t
-      | Object of Object.t
-
-    and var_t =
-      | Var
-      | Let
-      | Const
-  end
-
-  type t' = 
-    | Identifier of Identifier.t
-    | Expression of Expression.t
-    | Object of Object.t
-    | VariableDeclaration of VariableDeclaration.t
-  
-  type t = (t' * Loc.t)
-end = Statement
-*)
 
 
 
