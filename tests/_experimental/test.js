1a6c368536915bb749a24daf46b540e6dfcaa7af
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-05-27 15:03:00 -0400

    new target test

--- tests/_experimental/test.js
+++ tests/_experimental/test.js
@@ -1,6 +1,19 @@
-switch(true) {
-  case 1:
-    break;
-  case 2:
-    break;
+class Parent {
+    constructor() {
+        // implicit (from the `super` call)
+        //    new.target = Child;
+        // implicit (because `Parent` doesn't extend anything):
+        //    this = Object.create(new.target.prototype);
+        console.log(new.target) // Child!
+    }
 }
+class Child extends Parent {
+    constructor() {
+        // `this` is uninitialised (and would throw if accessed)
+        // implicit (from the `new` call):
+        //    new.target = Child 
+        super(); // this = Reflect.construct(Parent, [], new.target);
+        console.log(this);
+    }
+}
+new Child;
