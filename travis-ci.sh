2d3117ed543367529bf58e2f3b4a52bce24af381
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-07-03 20:11:01 -0700

    Update travis #3

--- travis-ci.sh
+++ travis-ci.sh
@@ -13,7 +13,7 @@ sudo apt-get install -qq ocaml
 curl -L https://github.com/OCamlPro/opam/archive/${OPAM_VERSION}.tar.gz | tar xz -C /tmp
 pushd /tmp/opam-${OPAM_VERSION}
 ./configure
-make lib-ext
+make
 sudo make install
 opam init
 eval `opam config -env`
