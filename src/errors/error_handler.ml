8fae5cbeae00208fa957f8228df0284b5fd638e5
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-08-28 20:56:17 -0400

    Add bool for early bail on expression parse

--- src/errors/error_handler.ml
+++ src/errors/error_handler.ml
@@ -1,6 +1,8 @@
 
 open Loc
 
+let use_inline_error_marking = false
+
 (* Reports a simple and general error *)
 let report ~msg ~level =
   match level with
@@ -17,7 +19,17 @@ let report ~msg ~level =
 
 (* Locates the offending area in the given source file, converts to a string and returns it. 
  * This string is generally thrown somewhere else. *)
-let exposed_error ~source ~loc ~msg =
+let rec exposed_error ~source ~loc ~msg =
+  if use_inline_error_marking <> true then
+    expose_error_fallback ~source:source ~loc:loc ~msg:msg
+  else
+    exposed_error_with_markings ~source:source ~loc:loc ~msg:msg
+
+(* Error message with physical markings to indicate an error.
+ * const foo = var
+ *             ^^^
+ *)
+and expose_error_fallback ~source ~loc ~msg =
   let source_path, source_file = Utils.depath source in
   let most_upper_line = ref "" in
   let upper_line = ref "" in
@@ -41,7 +53,7 @@ let exposed_error ~source ~loc ~msg =
     \x1b[31m  ● \x1b[39m%s\n\n\
     \x1b[90m%4d │ %s\n\
     \x1b[90m%4d │ %s\n\
-    \x1b[90m%4d │\x1b[39m %s\n\
+    \x1b[39m%4d │ %s\n\
    \x1b[90m     │\x1b[31m%s\x1b[39m\n\
     \x1b[90m%4d │ %s \x1b[39m\n\
     \x1b[90m%4d │ %s \x1b[39m\n"
@@ -57,6 +69,53 @@ let exposed_error ~source ~loc ~msg =
     loc.line
     !offending_line
     (spacing ^ arrow)
+    (loc.line + 1)
+    !lower_line
+    (loc.line + 2)
+    !most_lower_line
+
+(* Error message with colored markings to indicate an error.
+ * const foo = var
+ *              `-. var is red here
+ *)
+and exposed_error_with_markings ~source ~loc ~msg =
+  let source_path, source_file = Utils.depath source in
+  let most_upper_line = ref "" in
+  let upper_line = ref "" in
+  let offending_line = ref "" in
+  let lower_line = ref "" in
+  let most_lower_line = ref "" in
+  let lines = Batteries.File.lines_of source in
+  let _ = Batteries.Enum.fold 
+    (fun cur_line line -> 
+      if cur_line = (loc.line - 2) then most_upper_line := line else ();
+      if cur_line = (loc.line - 1) then upper_line := line else ();
+      if cur_line = loc.line then offending_line := line else ();
+      if cur_line = (loc.line + 1) then lower_line := line else ();
+      if cur_line = (loc.line + 2) then most_lower_line := line else ();
+      cur_line + 1
+    ) 1 lines
+  in Printf.sprintf "\
+    %s\x1b[1m%s\x1b[0m \x1b[90m(%d:%d)\x1b[39m\n\n\
+    \x1b[31m  ● \x1b[39m%s\n\n\
+    \x1b[90m%4d │ %s\n\
+    \x1b[90m%4d │ %s\n\
+    \x1b[39m%4d │ %s\x1b[31m%s\x1b[39m%s\n\
+    \x1b[90m%4d │ %s \x1b[39m\n\
+    \x1b[90m%4d │ %s \x1b[39m\n"
+    source_file
+    source_path
+    loc.line
+    loc.column
+    msg
+    (loc.line - 2)
+    !most_upper_line
+    (loc.line - 1)
+    !upper_line
+    loc.line
+    (String.sub !offending_line 0 (loc.column - 1))
+    (String.sub !offending_line (loc.column - 1) loc.length)
+    (String.sub !offending_line (loc.column - 1 + loc.length) ((String.length !offending_line) - (loc.column + loc.length) + 1))
     (loc.line + 1)
     !lower_line
     (loc.line + 2)
--- tests/_experimental/test.js
+++ tests/_experimental/test.js
@@ -1,28 +1,39 @@
 
 'use strict'
+
 // VariableDeclarations
 let foo = 1
+
 // VariableDeclarations
 let foo, bar = 1
+
 // BinaryExpressions and VariableDeclarations
-const foo, bar, baz = 1 - i + (3 * 4) / 5
+const foo, bar, baz = 1 - i + (3 * 5) / 5
+
 // CallExpressions and SpreadElements
 const item = foo((2 + 3), baz, 1 + 2, ...bar)
+
 // IdentifierExpression
 foo
+
 // AssignmentExpression
 foo = 1 + 2, 3, 4
-1
+
 // AssignmentExpression
 foo = bar
+
 // AssignmentExpression
 foo = false
+
 // AssignmentExpression with BinaryExpression
 foo = 1 - i + (3 * 4) / 5 
+
 // BinaryExpression with AssignmentExpression
 // a = 1 + b   ,   c    ,    d = e
 a = 1 + b, c, d = e
+
 // BinaryExpression with AssignmentExpression
 foo, bar = 2 + true
+
 // BinaryExpression with AssignmentExpression
 foo = bar = baz
