00879327d260ca3cf8244f234fe26874fc1964c1
Nicholas Zuber <zuber.nicholas@gmail.com>
2018-03-20 17:33:28 -0400

    More tests

--- tests/_experimental/test.js
+++ tests/_experimental/test.js
@@ -1,1 +1,13 @@
-var a = 1, true = b
+foo = 1
+foo = "a"
+foo = 1, 2
+foo = 1, 2, 3
+foo = "a", 2, 3
+foo = "a", 2, "b"
+
+foo = 1, bar = 1
+foo = "a", bar = 1
+foo = 1, 2, bar = 1
+foo = 1, 2, 3, bar = 1
+foo = "a", 2, 3, bar = 1
+foo = "a", 2, "b", bar = 1, 2, baz = 2
