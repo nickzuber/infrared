b203005ddf690bfe74326a44c767796dfe2b0f81
Nicholas Zuber <zuber.nicholas@gmail.com>
2018-03-22 21:12:36 -0400

    Add styling for coq proof viewer

--- proofs/binary_expression_commutativity.v
+++ proofs/binary_expression_commutativity.v
@@ -1,17 +1,33 @@
-Inductive nat : Set :=
-  | O : nat
-  | S : nat -> nat.
-
-Eval compute in O.
-Eval compute in (S O).
-
 Fixpoint add (a b : nat) : nat :=
   match a with
   | O => b
   | S n => S (add n b)
   end.
 
-Theorem add_is_assiciative: forall (a b c : nat),
+Fixpoint pred (n : nat) : nat :=
+  match n with
+  | O => O
+  | S n' => n'
+  end.
+
+Theorem zero_is_not_greater_than_zero : ~ 0 > 0.
+Proof.
+  unfold not.
+  unfold gt.
+  unfold lt.
+  intros H.
+  inversion H.
+Qed.
+
+Definition pred_arg_is_greater_than_zero (n : nat) : n > 0 -> nat :=
+  match n with
+  | O => fun pf => match (zero_is_not_greater_than_zero pf) with
+    (* match on all constructors of false, which is none *) end
+  | S n' => fun _ => n'
+  end.
+
+
+Theorem add_is_associative: forall (a b c : nat),
   (add a (add b c)) = (add (add a b) c).
 Proof.
   intros a b c.
@@ -22,3 +38,10 @@ Proof.
   rewrite IHa.
   reflexivity.
 Qed.
+
+Theorem negate: ~ 1 = 2.
+Proof.
+  unfold not.
+  intros H.
+  inversion H.
+Admitted.
