8252f947d5005bca1fe9c7cafb85bfb28b06bb6a
Nicholas Zuber <zuber.nicholas@gmail.com>
2018-07-04 01:43:13 -0400

    Add some basic error handling

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
+#!/usr/bin/env node
 'use strict';
 
 const fs = require('fs');
-const {
-  parseScriptWithLocation,
-  parseModuleWithLocation
-} = require("shift-parser");
+const { parseScriptWithLocation, parseModuleWithLocation } = require("shift-parser");
 
-const file = fs.readFile('./tests/experimental/index.js', 'utf8', (err, data) => {
-  if (err) throw err;
+function loadFile(file) {
+  const _ = fs.readFile(file, 'utf8', (loadingError, data) => {
+    if (loadingError) {
+      console.log(loadingError);
+      process.exit(1);
+    }
 
-  const {
-    tree: ast,
-    locations,
-    comments
-  } = parseModuleWithLocation(data);
+    try {
+      const {
+        tree: ast,
+        locations,
+        comments
+      } = parseModuleWithLocation(data);
 
-  console.log(JSON.stringify(ast, null, 4))
-  console.log(locations.get(ast.items[0]))
-});
+      console.log(JSON.stringify(ast, null, 4));
+      console.log(locations.get(ast.items[0]));
+    } catch (parsingError) {
+      console.log(`"${parsingError.description}" at ${parsingError.line}:${parsingError.column}`)
+    }
+  });
+}
+
+// @TEST
+// Mock run
+loadFile('./tests/experimental/index.js');
