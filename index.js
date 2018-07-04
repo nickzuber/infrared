ec90c2e53be6dd152bfada8dfd3532bfb9efed57
Nicholas Zuber <zuber.nicholas@gmail.com>
2018-07-03 20:05:54 -0400

    Start integrating parser

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('coming soon bay bee');
+const fs = require('fs');
+const {
+  parseScriptWithLocation,
+  parseModuleWithLocation
+} = require("shift-parser");
+
+const file = fs.readFile('./tests/experimental/index.js', 'utf8', (err, data) => {
+  if (err) throw err;
+
+  const {
+    tree: ast,
+    locations,
+    comments
+  } = parseModuleWithLocation(data);
+
+  console.log(JSON.stringify(ast, null, 4))
+  console.log(locations.get(ast.items[0]))
+});
---
+++ tests/experimental/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// The current time.
+const CURRENT_TIME = +Date.now();
