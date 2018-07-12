1f814cee72cbdca9c8d32b7930e5928519187ef4
Nicholas Zuber <zuber.nicholas@gmail.com>
2018-07-11 22:52:59 -0400

    Properly extend NativeEncoder module

--- infrared-core/src/typechecker/analyzer.ml
+++ infrared-core/src/typechecker/analyzer.ml
@@ -4,8 +4,8 @@ let check file =
   Printf.printf "checking.. %s" file;
   let parsetree = NativeEncoder.parse file in
   (* let open NativeEncoder in *)
-  let open Yojson.Basic.Util in
+  let open NativeEncoder.Util in
   let ast = parsetree |> member "tree" in
   let directives = ast |> member "directives" |> to_list in
-  let t = List.hd directives |> member "rawValue" |> to_string in
+  let t = List.hd directives |> member "type" |> to_string in
   print_endline ("\n--> " ^ t)
--- infrared-core/src/typechecker/encoder.ml
+++ infrared-core/src/typechecker/encoder.ml
@@ -1,37 +1,6 @@
 open Yojson
 
-module NativeEncoder : sig
-  val parse : string -> Yojson.Basic.json
-  val member : Yojson.Basic.json -> string -> Yojson.Basic.json
-  val to_string : Yojson.Basic.json -> string
-  val to_int : Yojson.Basic.json -> int
-  val to_bool : Yojson.Basic.json -> bool
-  val to_bool_option : Yojson.Basic.json -> bool option
-  val to_list : Yojson.Basic.json -> Yojson.Basic.json list
-end = struct
+module NativeEncoder = struct
+  include Yojson.Basic
   let parse file = Basic.from_file file
-
-  let member json key =
-    let open Basic.Util in
-    json |> member key
-
-  let to_string item =
-    let open Basic.Util in
-    item |> to_string
-
-  let to_int item =
-    let open Basic.Util in
-    item |> to_int
-
-  let to_bool item =
-    let open Basic.Util in
-    item |> to_bool
-
-  let to_bool_option item =
-    let open Basic.Util in
-    item |> to_bool_option
-
-  let to_list item =
-    let open Basic.Util in
-    item |> to_list
 end
