85177468232d1e4bfcff720bf637057871315e8e
Nicholas Zuber <zuber.nicholas@gmail.com>
2019-01-27 15:49:42 -0500

    Hook in parser with dune

--- __new-infrared__/Infrared/main.ml
+++ __new-infrared__/Infrared/main.ml
@@ -1,27 +1,6 @@
 open Core
+open InfraredParser
 
 let () =
-  let parse_source ~types_in_comments source =
-    let parse_options = Some Flow_parser.Parser_env.({
-        esproposal_optional_chaining = false;
-        esproposal_class_instance_fields = true;
-        esproposal_class_static_fields = true;
-        esproposal_decorators = true;
-        esproposal_export_star_as = true;
-        types = true;
-        types_in_comments;
-        use_strict = false;
-      })
-    in
-    Flow_parser.Parser_flow.program source ~parse_options
-  in
-  let source = "let x = 1; /* : */" in
-  let _ = parse_source ~types_in_comments:false source in
-  print_endline "types_in_comments=false: OK";
-  let () = try
-    parse_source ~types_in_comments:true source |> ignore
-  with
-  | Flow_parser.Parse_error.Error _ ->
-    print_endline "types_in_comments=true: OK";
-  in
+  let _ = Printf.printf "%d\n" Parser.x in
   ()
