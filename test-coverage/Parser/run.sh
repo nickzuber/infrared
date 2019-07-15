9e37e94a6b1dc575d59113c30b345d122f682855
Nicholas Zuber <zuber.nicholas@gmail.com>
2019-07-14 22:43:51 -0400

    Fix regex bug

--- test-coverage/Parser/run.sh
+++ test-coverage/Parser/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rgxFindFn="and ([a-zA-Z|_]+)"
-rgxFindUnfinishedMatchStmt="\s+| ([a-zA-Z]+) _ ->"
-rgxFindFinishedMatchStmt="\s+| ([a-zA-Z]+)"
+rgxFindFn="and (string_of_statement|string_of_expression) "
+rgxFindUnfinishedMatchStmt=":space:+| ([a-zA-Z]+) _ ->"
+rgxFindFinishedMatchStmt=":space:+| ([a-zA-Z]+)"
 rgxIsValidBlock="^[A-Z]"
 
 begin=0
@@ -15,28 +15,32 @@ fnPrinterCoverage () {
   while read -r line
   do
     if [[ begin -eq 1 && -z $line ]]; then
-      echo "pass $pass fail $fail"
+      [[ $fail -eq 0 ]]\
+        && local color="\033[32;1m"\
+        || local color="\033[31;1m"
+      local total=$((fail + pass))
+      echo "\n$color$pass/$total passing\033[39;0m"
       pass=0
       fail=0
+      begin=0
     elif [[ $line =~ $rgxFindFn ]]; then
       echo "\033[1m\n${BASH_REMATCH[1]}\033[0m"
       begin=1
     elif [[ begin -eq 1 && $line =~ $rgxFindUnfinishedMatchStmt ]]; then
       local name=${BASH_REMATCH[1]}
       if [[ -n $name ]]; then
-        echo " \033[31m×\033[39;0m $name"
+        echo " \033[90m↺ $name\033[39;0m"
         ((fail++))
-      else
-        [[ $line =~ $rgxFindFinishedMatchStmt ]]
-        local name=${BASH_REMATCH[1]}
-        if [[ -n $name && $name =~ $rgxIsValidBlock ]]; then
-          echo " \033[32;1m✓\033[39;0m $name"
-          ((pass++))
-        fi
+      fi
+    elif [[ begin -eq 1 && $line =~ $rgxFindFinishedMatchStmt ]]; then
+      local name=${BASH_REMATCH[1]}
+      if [[ -n $name && $name =~ $rgxIsValidBlock ]]; then
+        echo " \033[32;1m✓\033[39;0m $name"
+        ((pass++))
       fi
     fi
+    local name=""
   done < "$filePrinter"
-  begin=0
 }
 
 fnPrinterCoverage
