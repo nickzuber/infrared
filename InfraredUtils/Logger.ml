a3a69591e724f80d8bae2bddb8205be404dbd6ee
Nicholas Zuber <zuber.nicholas@gmail.com>
2021-05-07 19:49:24 -0400

    Print the file

--- InfraredUtils/Logger.ml
+++ InfraredUtils/Logger.ml
@@ -9,11 +9,11 @@ let warn (msg : string) (loc : Loc.t) : unit =
 
 let error (msg : string) (loc : Loc.t) : unit =
   Printf.printf "%s %s %s\n"
-    (" ⃠ " |> Chalk.red |> Chalk.bold)
+    ("  ⃠ " |> Chalk.red |> Chalk.bold)
     ((Printf.sprintf "(%d:%d)" loc.start.line loc._end.column) |> Chalk.gray)
     msg
 
 let error_no_loc (msg : string) : unit =
   Printf.printf "%s (?:?) %s\n"
-    (" ⃠ " |> Chalk.red |> Chalk.bold)
+    ("  ⃠ " |> Chalk.red |> Chalk.bold)
     msg
--- tests/experiment.js
+++ tests/experiment.js
@@ -1,17 +1,3 @@
-// const one = 1;
-
-// function doNothingInteresting (a, b) {
-//   function getNumberOne () {
-//     return 1;
-//   }
-//   if (Math.random() > 0) {
-//     return null
-//   }
-//   return getNumberOne();
-// }
-// doNothingInteresting(one, 2);
-// baz(one, 2);
-
 const foo = add1(5);
 
 function add1 (n) {
