73b6afb00679c9c56e230931fe24f5ca125986d2
Nicholas Zuber <zuber.nicholas@gmail.com>
2021-05-07 18:50:48 -0400

    Create flag in Settings

--- InfraredUtils/Settings.ml
+++ InfraredUtils/Settings.ml
@@ -1,2 +1,5 @@
 (* We print the intermediate steps and data structures when in debug mode. *)
 let debug_mode = ref false
+
+(* We print the inferred types in the console for each identifier in each file. *)
+let show_types_in_console = ref false
