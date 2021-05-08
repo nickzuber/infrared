e0af53fbb5db7fccba1161e537cf78214903ad90
Nicholas Zuber <zuber.nicholas@gmail.com>
2021-05-08 00:00:45 -0400

    Clean up experiment file

--- tests/experiment.js
+++ tests/experiment.js
@@ -1,29 +1,7 @@
-const age = 25;
+const count = 100;
 const foo = addFive(6);
 
 function addFive (n) {
   const five = 5;
   return n + five;
-}
-
-function doStuff (n) {
-  const five = 5;
-
-  function inner () {
-    return null;
-  }
-
-  if (n > 10) {
-    const ignore = undefined;
-    return null;
-  }
-
-  return n + five;
-}
-
-age + 1
-age
-
-function inner () {
-  return null;
 }
